2fb3902172482833269eb53400491b411d429bbb
Rav S <rsingh0815@gmail.com>
2026-02-01 18:50:36 -0500

    reverse linked list java

---
+++ JAVA/LINKED_LIST/reverse_linked.java
@@ -0,0 +1,22 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { 
+        val = x;
+        next = null;
+    }
+}
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+}
