8a7568c6e5e7ff14870a344849d29710dd4ff8c6
Rav S <rsingh0815@gmail.com>
2026-03-24 18:50:41 -0400

    palindrome in JS

---
+++ JAVASCRIPT/MATH/palindrome_number.js
@@ -0,0 +1,13 @@
+function isPalindrome(x) {
+    if (x < 0) return false;
+    const original = x;
+    let reversed = 0;
+
+    while (x > 0) {
+        const digit = x % 10;
+        reversed = reversed * 10 + digit;
+        x = Math.floor(x/10);
+    };
+
+    return reversed === original;
+};
