a901f20e497a6edb831711a10307c985074ae32c
Rav S <rsingh0815@gmail.com>
2026-04-01 01:53:09 -0400

    maximum subarray in js

---
+++ JAVASCRIPT/ARRAYS/maximum_subarray.js
@@ -0,0 +1,9 @@
+function maxSubArray(nums) {
+    let currentSum = nums[0];
+    let maxSum = nums[0];
+    for (let i=1; i<nums.length; i++) {
+        currentSum = Math.max(nums[i], currentSum + nums[i]);
+        maxSum = Math.max(currentSum, maxSum);
+    }
+    return maxSum;
+}
